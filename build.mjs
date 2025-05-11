31ea3172868f1c585691ee5d1029131acb93ee66
Mkamvi <karl.meng@qq.com>
2025-05-11 18:25:23 +0800

    feat: 增加交易日志模版

--- build.mjs
+++ build.mjs
@@ -8,7 +8,19 @@ const today = new Date();
 const year = today.getFullYear();
 const month = (today.getMonth() + 1).toString().padStart(2, "0"); // 月份补零
 const day = today.getDate().toString().padStart(2, "0"); // 日期补零
-const formattedDate = `daily/${year}/${month}/${day}`;
-const title = `${year}年${month}月${day}日`;
 
-await $`hexo new daily ${title} -p ${formattedDate}`.pipe(process.stdout);
+const layout = process.env.layout;
+const formattedDate = `${year}/${month}/${day}`;
+const title = formattedDate.replaceAll("/", "-");
+
+let postPath = "";
+switch (layout) {
+  case "more":
+    postPath = `livermore/${formattedDate}`;
+    break;
+  default:
+    postPath = `daily/${formattedDate}`;
+    break;
+}
+
+await $`hexo new ${layout} ${title} -p ${postPath}`.pipe(process.stdout);
---
+++ scaffolds/more.md
@@ -0,0 +1,8 @@
+---
+title: '{{ title }} - 股票操作日志'
+date: {{ date }}
+tags:
+  - 股票操作日志
+categories:
+  - 股票操作日志
+---
